41c97d1657acfba8ac8d13d4a5f26de34ce1fc86
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-07-27 13:57:45 -0300

    add_empresaDB

---
+++ db/empresa.sql
@@ -0,0 +1,126 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: empresa
+-- ------------------------------------------------------
+-- Server version	8.0.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `controla`
+--
+
+LOCK TABLES `controla` WRITE;
+/*!40000 ALTER TABLE `controla` DISABLE KEYS */;
+/*!40000 ALTER TABLE `controla` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `departamento`
+--
+
+LOCK TABLES `departamento` WRITE;
+/*!40000 ALTER TABLE `departamento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `departamento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `dependente`
+--
+
+LOCK TABLES `dependente` WRITE;
+/*!40000 ALTER TABLE `dependente` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dependente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `funcionario`
+--
+
+LOCK TABLES `funcionario` WRITE;
+/*!40000 ALTER TABLE `funcionario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `funcionario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `gerencia`
+--
+
+LOCK TABLES `gerencia` WRITE;
+/*!40000 ALTER TABLE `gerencia` DISABLE KEYS */;
+/*!40000 ALTER TABLE `gerencia` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `localizacao`
+--
+
+LOCK TABLES `localizacao` WRITE;
+/*!40000 ALTER TABLE `localizacao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `localizacao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `nome`
+--
+
+LOCK TABLES `nome` WRITE;
+/*!40000 ALTER TABLE `nome` DISABLE KEYS */;
+/*!40000 ALTER TABLE `nome` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `projeto`
+--
+
+LOCK TABLES `projeto` WRITE;
+/*!40000 ALTER TABLE `projeto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `projeto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `supervisiona`
+--
+
+LOCK TABLES `supervisiona` WRITE;
+/*!40000 ALTER TABLE `supervisiona` DISABLE KEYS */;
+/*!40000 ALTER TABLE `supervisiona` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `trabalha_em`
+--
+
+LOCK TABLES `trabalha_em` WRITE;
+/*!40000 ALTER TABLE `trabalha_em` DISABLE KEYS */;
+/*!40000 ALTER TABLE `trabalha_em` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `trabalha_para`
+--
+
+LOCK TABLES `trabalha_para` WRITE;
+/*!40000 ALTER TABLE `trabalha_para` DISABLE KEYS */;
+/*!40000 ALTER TABLE `trabalha_para` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-07-27 13:57:11
